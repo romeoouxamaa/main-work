f1c6cdd2387ee7bb6b8387e2f7bb446e12983a87
romeoouxamaa <romeoouxamaa@gmail.com>
2022-10-15 23:41:55 -0700

    no chnage

--- main work.dart
+++ main work.dart
@@ -6,4 +6,31 @@ void main(){
   print(num1==num2 && num3==num4);
 print(num1==num2 || num3==num4);
 print(num1==num2 && !(num3==num4));
+int english=34;
+int maths=45;
+int biology=76;
+int science=26;
+print("english : $english / 100");
+print("maths : $maths / 100 ");
+print("biology : $biology / 100 ");
+print("sceince : $science / 100");
+int percnetgae =90;
+int newnum=56;
+if(percnetgae>=80){
+  int newnum=10;
+  print("$newnum A +");
+  else if(percnetgae>=70){
+    int newnum=20;
+    print("$newnum A ");
+  }else if(percnetgae>=60){
+    int newnum=30;
+    print("$newnum C");
+    }
+    int a=45;
+    int b=98;
+    print("multiplication : ${a * b }");
+print("addition : ${a + b }");
+print("division : ${a / b }");
+print("return int : ${a~/b}");
+print("reminder : ${a%b }")l;
 }
